bff7d84e5f232bf007a41feb3c1f38cb3d75e216
MyGithAr <arpiginanjar123@gmail.com>
2022-02-09 12:20:52 +0700

    portofolio pbo

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,7 +11,7 @@ function HeroSection() {
              saya seorang front-end engineer , back-end egineer, dan juga UI designer
              </h1>
              <p className="mt-6 text-lg text-opasity text-zinc-400 w-4/12 mx-auto my-6 ">
-             sejak 13 tahun saya memulai programing. sejak 3 tahun memulqai UI design.
+             sejak 17 tahun saya memulai programing. sejak 2 tahun memulqai UI design.
              </p>
 <Button >
            pelajari
